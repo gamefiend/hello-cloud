352275390453ba5bddd3c30d1c5159b51cbe8d03
Quinn Murphy <gamefiend@github.com>
2017-05-08 15:37:20 -0500

    latest/greatest

--- part1/tutorial.tf
+++ part1/tutorial.tf
@@ -1,4 +1,4 @@
-#  Tutorial.tf
+# Tutorial.tf (1-1)
 # For sake of simplicity, we are putting everything in one file
 # Normally, you will break each section into its own file for better
 # organization. Each section will include the suggested file name
@@ -13,14 +13,12 @@
 ### Credentials for different providers we will use.
 
 provider "aws" {
-  # You can delete this entry if you have it defined in ~/.aws/credentials
-  # file or if you export AWS_DEFAULT_REGION as environment variable
-  region     = "<YOUR REGION HERE>"
+  region     = "us-east-1"
 
   # You can delete these entries if you have ~/.aws/credentials file
   # or you export AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
-  access_key = "<YOUR KEY HERE>"
-  secret_key = "<YOUR SECRET HERE>"
+  #access_key = "<YOUR KEY HERE>"
+  #secret_key = "<YOUR SECRET HERE>"
 }
 
 ### Network
@@ -40,99 +38,40 @@ resource "aws_internet_gateway" "tutorial" {
   vpc_id = "${aws_vpc.tutorial.id}"
 }
 
+resource "aws_route_table" "tutorial" {
+  vpc_id = "${aws_vpc.tutorial.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.tutorial.id}"
+    }
+
+  tags {
+    Name = "hello-cloud"
+    }
+}
+
+resource "aws_route_table_association" "tutorial" {
+  route_table_id = "${aws_route_table.tutorial.id}"
+  subnet_id = "${aws_subnet.tutorial.id}"
+}
+
+
+
 resource "aws_security_group" "tutorial_http" {
   vpc_id = "${aws_vpc.tutorial.id}"
 
   ingress {
-    cidr_block =
+    cidr_blocks = ["0.0.0.0/0"]
     from_port = 0
     to_port = 80
-    protocol = tcp
+    protocol = "tcp"
     }
 }
 
-### S3
-### file s3.tf
-### S3 bucket for site index page
-
-resource "aws_s3_bucket" "hellocloud-tutorial-site-bucket" {
-    bucket = "hellocloud-tutorial-site"
-    acl = "public-read"
-    policy = <<EOF
-{
-  "Id": "Policy1462998290938",
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "Stmt1462998074852",
-      "Action": [
-        "s3:GetObject"
-      ],
-      "Effect": "Allow",
-      "Resource": "arn:aws:s3:::hellocloud-tutorial-site/*",
-      "Principal": "*"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_s3_bucket_object" "site_object" {
-    depends_on = ["aws_s3_bucket.hellocloud-tutorial-site-bucket"]
-    bucket = "hellocloud-tutorial-site"
-    key = "index.html"
-    source = "s3/files/site/index.html"
-    etag = "${md5(file("s3/files/site/index.html"))}"
-}
-
-### IAM
-### file iam-roles.tf
-### IAM profile and policy for S3 bucket access
-
-resource "aws_iam_instance_profile" "bucket_access_instance_profile" {
-  name = "bucket_access_instance_profile"
-  roles = ["${aws_iam_role.bucket_access_role.name}"]
-}
-
-resource "aws_iam_role" "bucket_access_role" {
-  name = "bucket_access_role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy" "bucket_access_policy" {
-  name = "bucket_access_policy"
-  role = "${aws_iam_role.bucket_access_role.id}"
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "s3:*",
-      "Resource": ["arn:aws:s3:::${var.s3SiteBucket}"]
-    },
-    {
-      "Effect": "Allow",
-      "Action": "s3:*",
-      "Resource": ["arn:aws:s3:::${var.s3SiteBucket}/*"]
-    }
-  ]
-}
-EOF
+resource "aws_eip" "tutorial" {
+  instance = "${aws_instance.tutorial.id}"
+  vpc = true
 }
 
 
@@ -141,42 +80,12 @@ EOF
 ### EC2 compute resources
 
 resource "aws_instance" "tutorial" {
-  # TODO: create ami with apache and very basic condition
-  ami = "${var.ami}"
-  instance_type = "t2.micro"
-  associate_public_ip_address = true
+  ami = "ami-fe2c59e8"
+  instance_type = "t2.nano"
+  subnet_id = "${aws_subnet.tutorial.id}"
+  vpc_security_group_ids = ["${aws_security_group.tutorial_http.id}"]
   tags {
-  		Name = "web"
+  		Name = "hello-cloud"
   }
-  iam_instance_profile = "${aws_iam_instance_profile.bucket_access_instance_profile.name}"
-  key_name = "${var.key_name}"
-
-  connection {
-  	user = "ubuntu"
-  	type = "ssh"
-  	key_file = "${var.key_file}"
-  	timeout = "2m"
-  }
-  provisioner "remote-exec" {
-    	inline = [
-      "sudo apt-get update",
-      "sudo apt-get -y install awscli",
-      "sudo apt-get -y install http2",
-      "sudo systemctl restart apache2",
-    	"sudo mkdir -p /var/www/html/",
-    	"sudo aws s3 cp s3://${var.s3SiteBucket}/index.html /var/www/html/ --region=us-west-2"
-    	]
-    }
 }
 
-### Route53
-### file route53.tf
-### Route 53 record for Web Server
-
-resource "aws_route53_record" "web" {
-	zone_id = "PLACEHOLDER"
-	name = "web.hellocloud-tutorial.io"
-	type = "CNAME"
-	ttl = "300"
-	records = ["${aws_instance.tutorial.public_dns}"]
-}
--- part2/tutorial.tf
+++ part2/tutorial.tf
@@ -1,4 +1,4 @@
-#  Tutorial.tf
+# Tutorial.tf (1-1)
 # For sake of simplicity, we are putting everything in one file
 # Normally, you will break each section into its own file for better
 # organization. Each section will include the suggested file name
@@ -8,19 +8,25 @@
 ### file: variables.tf
 ### User-defined variables, if any
 
+variable "size" {
+  type = "map"
+  default = {
+    "small" = 1
+    "big" = 3
+    }
+  }
+
 ### Providers
 ### file:providers.tf
 ### Credentials for different providers we will use.
 
 provider "aws" {
-  # You can delete this entry if you have it defined in ~/.aws/credentials 
-  # file or if you export AWS_DEFAULT_REGION as environment variable
-  region     = "<YOUR REGION HERE>"
+  region     = "us-east-1"
 
   # You can delete these entries if you have ~/.aws/credentials file
   # or you export AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
-  access_key = "<YOUR KEY HERE>"
-  secret_key = "<YOUR SECRET HERE>"
+  #access_key = "<YOUR KEY HERE>"
+  #secret_key = "<YOUR SECRET HERE>"
 }
 
 ### Network
@@ -40,20 +46,65 @@ resource "aws_internet_gateway" "tutorial" {
   vpc_id = "${aws_vpc.tutorial.id}"
 }
 
+resource "aws_route_table" "tutorial" {
+  vpc_id = "${aws_vpc.tutorial.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.tutorial.id}"
+    }
+
+  tags {
+    Name = "hello-cloud"
+    }
+}
+
+resource "aws_route_table_association" "tutorial" {
+  route_table_id = "${aws_route_table.tutorial.id}"
+  subnet_id = "${aws_subnet.tutorial.id}"
+}
+
+
+
 resource "aws_security_group" "tutorial_http" {
   vpc_id = "${aws_vpc.tutorial.id}"
 
   ingress {
-    cidr_block = 
+    cidr_blocks = ["0.0.0.0/0"]
     from_port = 0
     to_port = 80
-    protocol = tcp
+    protocol = "tcp"
     }
 }
+
+resource "aws_eip" "tutorial" {
+  instance = "${aws_instance.tutorial.id}"
+  vpc = true
+}
+
 
 ### Instances
 ### file: instances.tf
 ### EC2 compute resources
 
 resource "aws_instance" "tutorial" {
+  ami = "ami-fe2c59e8"
+  instance_type = "t2.nano"
+  subnet_id = "${aws_subnet.tutorial.id}"
+  vpc_security_group_ids = ["${aws_security_group.tutorial_http.id}"]
+  tags {
+  		Name = "hello-cloud"
+  }
+  count = ${lookup(var.size, 
+}
 
+### Outputs
+### file: outputs.tf
+### displays terraform variables
+
+output "elastic_ip" {
+  value = "${aws_eip.tutorial.public_ip}"
+}
+
+output "private_ip" {
+  value = "${aws_instance.tutorial.private_ip}"
